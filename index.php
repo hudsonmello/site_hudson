d23741d8f96cd69ab615929b52a5218b61febe6d
Hudson Mello <hudsonmello03@gmail.com>
2021-05-15 13:25:37 -0300

    estruturando a pagina de noticias e beneficios

--- index.php
+++ index.php
@@ -21,14 +21,13 @@
                 <nav>
                     <ul>
                         <li><a class="active" href="#">Home</a></li>
-                        <li><a href="#">Serviços</a></li>
+                        <li><a href="#">Serviï¿½os</a></li>
                         <li><a href="#">Projetos</a></li>
                         <li><a href="#">Contato</a></li>
                     </ul>
                 </nav>
             </div>
         </div>
-
     </header>
 
     <section class="banner">
@@ -36,17 +35,14 @@
             <div class="banner_headline">
                 <h1>Hudson Mello</h1>
                 <h2>Desenvolvedor Full Stack</h2>
-
             </div>
 
             <div class="banner_options">
                 <div class="banner_1">
                     <div class="banner_title">Sites e Landing Pages</div>
-                    <div class="banner_desc">Responsivos e adequados ao seu negócio</div>
+                    <div class="banner_desc">Responsivos e adequados ao seu negï¿½cio</div>
                     <a href="">Saiba Mais</a>
                 </div>
-
-
 
                 <div class="banner_2">
                     <div class="banner_title">Sistemas web</div>
@@ -54,28 +50,65 @@
                     <a href="">Saiba Mais</a>
                 </div>
 
-
                 <div class="banner_3">
-                    <div class="banner_title">Consultoria de Negócios
+                    <div class="banner_title">Consultoria de Negï¿½cios
                     </div>
-                    <div class="banner_desc">Análise de negócio voltada para seu crescimento</div>
+                    <div class="banner_desc">Anï¿½lise de negï¿½cio voltada para seu crescimento</div>
                     <a href="">Saiba Mais</a>
                 </div>
             </div>
-
         </div>
 
     </section>
 
     <section class="geral">
         <div class="container">
-            testando ....
+            <section>
+                <div class="widget">
+                    <div class="widget_title">
+                        <div class="widget_title_text">Ãšltimas NotÃ­cias
+                        </div>
+                        <div class="widget_title_bar"></div>
+                    </div>
+                    <div class="widget_body">
+                        Conteudo benefÃ­cios
+                    </div>
+                </div>
+            </section>
+
+            <aside>
+                <div class="widget">
+                    <div class="widget_title">
+                        <div class="widget_title_text">BenefÃ­cios</div>
+                        <div class="widget_title_bar"></div>
+                    </div>
+                    <div class="widget_beneficios">
+                            <div class="widget_title_box"></div>
+                            <div class="widget_title_box"></div>
+                            <div class="widget_title_box"></div>
+                            <div class="widget_title_box"></div>
+                            <div class="widget_title_box"></div>
+                            <div class="widget_title_box"></div>
+                            <div class="widget_title_box"></div>
+                            <div class="widget_title_box"></div>
+                            <div class="widget_title_box"></div>
+                    </div>
+                </div>
+                <div class="widget">
+                    <div class="widget_title">
+                        <div class="widget_title_text">FaÃ§a Seu orÃ§amento</div>
+                        <div class="widget_title_bar"></div>
+                    </div>
+                    <div class="widget_body">
+                        <h2>E-mail</h2>
+                        <p>hudsonmello03@gmail.com</p>
+                        <h2>Telefone</h2>
+                        <p>(43) 99192-7450</p>    
+                    </div>
+                </div>
+
+            </aside>
         </div>
-
-
-
-
-
     </section>
 </body>
 
