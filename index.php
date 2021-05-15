82565cbdfdf2ee2881938dbbb12392f00bc1eace
Hudson Mello <hudsonmello03@gmail.com>
2021-05-15 12:44:44 -0300

    website personal start

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hudson Mello</title>
+    <link rel="stylesheet" href="assets/css/template.css">
+    <link rel="shortcut icon" href="assets/images/favicon.png" type="image/x-icon">
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div class="logo">
+                <a href=""><img src="assets/images/logo.png" alt="" srcset=""></a>
+            </div>
+
+            <div class="menu">
+                <nav>
+                    <ul>
+                        <li><a class="active" href="#">Home</a></li>
+                        <li><a href="#">Serviços</a></li>
+                        <li><a href="#">Projetos</a></li>
+                        <li><a href="#">Contato</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+
+    </header>
+
+    <section class="banner">
+        <div class="container column">
+            <div class="banner_headline">
+                <h1>Hudson Mello</h1>
+                <h2>Desenvolvedor Full Stack</h2>
+
+            </div>
+
+            <div class="banner_options">
+                <div class="banner_1">
+                    <div class="banner_title">Sites e Landing Pages</div>
+                    <div class="banner_desc">Responsivos e adequados ao seu negócio</div>
+                    <a href="">Saiba Mais</a>
+                </div>
+
+
+
+                <div class="banner_2">
+                    <div class="banner_title">Sistemas web</div>
+                    <div class="banner_desc">Para gerenciar e aumentar seu crescimento</div>
+                    <a href="">Saiba Mais</a>
+                </div>
+
+
+                <div class="banner_3">
+                    <div class="banner_title">Consultoria de Negócios
+                    </div>
+                    <div class="banner_desc">Análise de negócio voltada para seu crescimento</div>
+                    <a href="">Saiba Mais</a>
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+
+    <section class="geral">
+        <div class="container">
+            testando ....
+        </div>
+
+
+
+
+
+    </section>
+</body>
+
+</html>
